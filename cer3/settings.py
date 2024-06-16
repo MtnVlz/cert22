ad8f45ba6a21f04e1da4bc265c6d9b8e05cc547d
Martin <mtn.vlzuela@gmail.com>
2024-06-16 03:30:19 -0400

    Actualizacion archivos

--- cer3/settings.py
+++ cer3/settings.py
@@ -73,11 +73,10 @@ WSGI_APPLICATION = 'cer3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://postgres:miau123.@cer3_db:5353/cer3')
 }
 
 
